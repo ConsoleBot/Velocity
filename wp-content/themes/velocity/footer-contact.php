8a8f795b3257841577e4906b20908d3a9f0e922f
ConsoleBot <34120869+ConsoleBot@users.noreply.github.com>
2025-09-04 04:25:38 +0800

    updates

---
+++ wp-content/themes/velocity/footer-contact.php
@@ -0,0 +1,75 @@
+    
+        <footer class="mt-auto">
+          <section class="relative">
+            <?php
+              $page_id = 69;
+              $footer_background = get_field('footer_background', $page_id);
+      
+              if ($footer_background) {
+                echo '<div class="z-[0] bg-cover absolute top-0 left-0 w-full h-full pointer-events-none" style="background-image: url(' . esc_url($footer_background) . ');"></div>';
+              } 
+            ?>
+            <div class="container relative z-1">
+              <div class="py-[64px] pb-[32px] md:py-[80px] flex md:items-center justify-between gap-[48px] md:gap-[16px] flex-col md:flex-row">
+                <div class="max-w-[384px]">
+                  <?php
+                    $page_id = 69;
+                    $header_logo_id = get_field('logo_light', $page_id);
+                    $logo = wp_get_attachment_image_src($header_logo_id, 'full'); 
+            
+                    if ($logo[0]) {
+                        echo '<a href="' . esc_url(home_url('/')) . '" data-animate="animate-fade-up" class="block mb-[24px]">';
+                        echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+                        echo '</a>';
+                    } else {
+                        echo '<h1 class="site-title"><a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h1>';
+                    }
+                  ?>
+                  <div data-animate="animate-fade-down animate-delay-[0ms]" class="text-[16px] text-white leading-[24px] tracking-[0.4px]">
+                    Velocity Properties, a product of Bermwood Consulting LLC
+                  </div>
+                  <div data-animate="animate-fade-right animate-delay-[200ms]" class="my-[24px] border-t-1 border-[#FEFEFE] w-[48px]"></div>
+                  <div data-animate="animate-fade-down animate-delay-[400ms]" class="text-[16px] text-white leading-[24px] tracking-[0.4px] mb-[8px]">
+                    3625 N. Janssen Avenue, Chicago, IL 60613
+                  </div>
+                  <div data-animate="animate-fade-down animate-delay-[600ms]" class="text-[16px] text-white leading-[24px] tracking-[0.4px]">
+                    (312) 543-6986 | info@velocity.properties
+                  </div>
+                </div>
+                <div class="w-full md:max-w-[690px] bg-[#00C4E3] flex-1 rounded-[16px] min-h-[368px] relative overflow-hidden">
+                  <?php
+                    $page_id = 69;
+
+                    $footer_contact_overlay = get_field('footer_contact_overlay', $page_id);
+
+                    if ($footer_contact_overlay) {
+                      echo '<div class="w-[277.24px] h-[356.78px] absolute bottom-[-66px] bg-center left-[-30px] bg-no-repeat" data-animate="animate-fade-up animate-once" style="background-image: url(' . esc_url($footer_contact_overlay) . ')"></div>';
+                    }
+                  ?>
+                  <?php
+                    $page_id = 69;
+
+                    $footer_contact_overlay = get_field('footer_contact_overlay', $page_id);
+
+                    if ($footer_contact_overlay) {
+                      echo '<div class="w-[277.24px] h-[356.78px] absolute bottom-[-66px] bg-center right-[-115px] bg-no-repeat" data-animate="animate-fade-up animate-once" style="background-image: url(' . esc_url($footer_contact_overlay) . ')"></div>';
+                    }
+                  ?>
+                  <?php
+                    $page_id = 69;
+                    $footer_contact_photo = get_field('footer_contact_photo', $page_id);
+            
+                    if ($footer_contact_photo) {
+                      echo '<div class="w-full h-full absolute bg-bottom bg-no-repeat bg-contain" data-animate="animate-fade-up animate-once" style="background-image: url(' . esc_url($footer_contact_photo) . ');"></div>';
+                    } 
+                  ?>
+                </div>
+              </div>
+              <p data-animate="animate-fade-down animate-once" class="py-[30px] border-t-1 border-[#323232] text-center text-white text-[16px] leading-[26px] tracking-[0.4px] font-semibold">&copy; 2025 Velocity Properties. All rights reserved.</p>
+            </div>
+          </section>
+        </footer>
+      </div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- wp-content/themes/velocity/template-contact.php
+++ wp-content/themes/velocity/template-contact.php
@@ -4,31 +4,49 @@
    */
   get_header();
   ?>
-  <section>
-    <div class="container pt-[158px]!"> 
-      <div class="flex">
+  <section class="relative overflow-hidden">
+    <?php
+      $page_id = 29;
+
+      $contact_hero_overlay = get_field('contact_hero_overlay', $page_id);
+
+      if ($contact_hero_overlay) {
+        echo '<div class="h-full bg-no-repeat bg-center bg-cover top-0 right-0 left-0 absolute" data-animate="animate-shake animate-once animate-duration-[4000ms]" style="background-image: url(' . esc_url($contact_hero_overlay) . ')"></div>';
+      }
+    ?>
+    <?php
+      $page_id = 29;
+
+      $contact_hero_photo = get_field('contact_hero_photo', $page_id);
+
+      if ($contact_hero_photo) {
+        echo '<div class="w-[145%] lg:w-auto h-full bg-no-repeat bg-bottom bg-cover bottom-0 right-0 left-0 absolute" data-animate="animate-fade-up animate-once" style="background-image: url(' . esc_url($contact_hero_photo) . ')"></div>';
+      }
+    ?>
+    <div class="relative container pt-[87px]! lg:pt-[158px]! pb-[190px]! lg:pb-[225px]!"> 
+      <div class="flex flex-col lg:flex-row gap-[41px] lg:gap-0">
         <div class="flex-1">
           <button data-animate="animate-fade-up animate-once animate-fill-both animate-duration-[1000ms]" class="mb-[16px] border-[#00C4E3] border bg-[#00C4E31A] rounded-[100px] py-[4px] sm:py-[8px] px-[12px] sm:px-[32px] font-semibold text-[10px] sm:text-[12px] tracking-[2px] text-[#00C4E3]">CONTACT</button>
-          <h1 class="max-w-[486px] text-[56px] text-[#191919] font-extrabold tracking-[0.2px] mb-[16px]">Connect with Our Team</h1>
-          <p class="max-w-[486px] font-inter text-[16px] text-[#4A5C60] leading-[157%] tracking-[0.15px]">Need support or have inquiries about our property tech solutions? We're here to assist you. Get in touch with our team using the contact form below, and we'll respond promptly to help with your needs.</p>
+          <h1 data-animate="animate-fade-up animate-once animate-fill-both animate-duration-[1000ms]" class="lg:max-w-[486px] text-[40px] lg:text-[56px] text-[#191919] font-extrabold tracking-[0.2px] mb-[16px]">Connect with Our Team</h1>
+          <p data-animate="animate-fade-up animate-once animate-fill-both animate-duration-[1000ms]" class="lg:max-w-[486px] font-inter text-[16px] text-[#4A5C60] leading-[157%] tracking-[0.15px]">Need support or have inquiries about our property tech solutions? We're here to assist you. Get in touch with our team using the contact form below, and we'll respond promptly to help with your needs.</p>
         </div>
-        <div class="max-w-[588px] contact-form flex-1 bg-[#FFFFFF] border-1 border-[#CCD6E0] rounded-[16px] py-[56px] px-[51px]">
+        <div data-animate="animate-fade animate-once animate-fill-both animate-duration-[1000ms]" class="lg:max-w-[588px] contact-form flex-1 bg-[#FFFFFF] border-1 border-[#CCD6E0] rounded-[16px] py-[32px] px-[24px] lg:py-[56px] lg:px-[51px]">
           <?php echo do_shortcode('[contact-form-7 id="8c9786f" title="Contact"]'); ?>
         </div>
       </div>
     </div>
   </section>
   <section class="bg-[#FEFEFE]">
-    <div class="container px-0! md:px-[16px]!">
-      <div class="pb-[173px] sm:pb-[151px] lg:pb-[151px] lg:py-[160px] flex justify-end">
-        <div class="flex justify-center items-center md:block py-[64px] pb-[320px] md:pt-[56px] px-[56px] md:pl-[0] md:pr-[56px] lg:pr-[78px] mr-[0] lg:mr-[102px] border-2 border-[#FEFEFE4D] rounded-0 md:rounded-[16px] h-full md:h-[327px] w-full md:w-[914px] bg-[#233134] relative">
+    <div class="container px-0! md:px-[16]!">
+      <div class="pb-[173px] sm:pb-[151px] lg:py-[80px] flex justify-end">
+        <div class="flex justify-center items-center md:block py-[64px] pb-[320px] md:pt-[56px] px-[56px] md:pl-[0] md:pr-[56px] lg:pr-[78px] mr-[0] lg:mr-[102px] border-2 border-[#FEFEFE4D] rounded-0 md:rounded-[16px] h-full md:h-[327px] w-full md:w-[914px] bg-[#00C4E3] relative">
           <?php
             $page_id = 69;
 
             $counter_background_overlay = get_field('counter_background_overlay', $page_id);
 
             if ($counter_background_overlay) {
-              echo '<div data-animate="animate-shake animate-once animate-duration-[4000ms]" class="top-0 opacity-[.2] absolute pointer-events-none h-full w-full bg-center bg-no-repeat bg-cover" style="background-image: url(' . esc_url($counter_background_overlay) . ')"></div>';
+              echo '<div data-animate="animate-shake animate-once animate-duration-[4000ms]" class="top-0 absolute pointer-events-none h-full w-full bg-center bg-no-repeat bg-cover" style="background-image: url(' . esc_url($counter_background_overlay) . ')"></div>';
             }
           ?>
           <?php
@@ -37,7 +55,7 @@
             $counter_logo = get_field('counter_logo', $page_id);
 
             if ($counter_logo) {
-              echo '<div data-animate="animate-fade-up animate-once" class="pointer-events-none absolute w-[100%] md:w-[719.9px] h-[477px] md:h-[784.23px] top-[unset] bottom-[-24%] md:top-[-300px] left-[unset] md:left-[-370px] lg:left-[-240px] bg-size-[180%] md:bg-size-[auto] bg-[56%_32%] bg-no-repeat" style="background-image: url(' . esc_url($counter_logo) . ')"></div>';
+              echo '<div data-animate="animate-fade-up animate-once" class="absolute w-[100%] md:w-[719.9px] h-[477px] md:h-[784.23px] top-[unset] bottom-[-24%] md:top-[-300px] left-[unset] md:left-[-370px] lg:left-[-240px] bg-size-[180%] md:bg-size-[auto] bg-[56%_32%] bg-no-repeat" style="background-image: url(' . esc_url($counter_logo) . ')"></div>';
             }
           ?>
           <div class="w-initial md:w-[432px] float-none md:float-right flex flex-col md:flex-row flex-no-wrap md:flex-wrap gap-x-[68px] gap-y-[24px] md:gap-y-[80px]">
@@ -62,4 +80,4 @@
       </div>
     </div>
   </section>
-<?php get_footer(); ?>
+<?php get_footer('contact'); ?>
